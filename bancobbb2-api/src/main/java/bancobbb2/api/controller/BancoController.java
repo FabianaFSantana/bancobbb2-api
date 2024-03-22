1d72e974fa922c3d23d6ff3ed6a9fb4992b71b89
Fabiana <fabiana.lua@gmail.com>
2024-03-22 20:55:39 -0300

    BBB-002 Criação do endpoint Get para localizar banco pelo id na classe BancoController

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/BancoController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/BancoController.java
@@ -1,11 +1,13 @@
 package bancobbb2.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +38,14 @@ public class BancoController {
                 .body(bancoRepository.findAll());
     }
 
-    
+    // Para Localizar o banco pelo id
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Banco>> localizarBancoPeloId(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(bancoRepository.findById(id));
+    }
+
+
 
     
 }
