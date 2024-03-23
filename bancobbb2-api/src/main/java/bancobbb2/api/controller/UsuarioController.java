2543aaf73810905a6700bb9ea02c1c52872690a1
Fabiana <fabiana.lua@gmail.com>
2024-03-23 01:10:51 -0300

    BBB-004 Criação do endpoint POST para cadastrar usuário na classe UsuarioController

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/UsuarioController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package bancobbb2.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bancobbb2.api.model.Usuario;
 import bancobbb2.api.repository.UsuarioRepository;
 
 @RestController
@@ -13,6 +18,13 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(usuarioRepository.save(usuario));
+    }
+
     
+
     
 }
