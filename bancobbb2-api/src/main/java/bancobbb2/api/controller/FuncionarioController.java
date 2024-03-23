cd7a71f6f4a4461b4048b7ab93d1423ad52e0aa1
Fabiana <fabiana.lua@gmail.com>
2024-03-22 23:58:51 -0300

    BBB-003 Criação do endpoint GET para buscar funcionario pelo id na classe FuncionarioController

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/FuncionarioController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/FuncionarioController.java
@@ -1,11 +1,13 @@
 package bancobbb2.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,13 @@ public class FuncionarioController {
         return ResponseEntity.status(HttpStatus.OK)
         .body(funcionarioRepository.findAll());
     }
+
+    //Para acessar funcionário pelo id
+    @GetMapping("/{idFuncionario}")
+    public ResponseEntity<Optional<Funcionario>> buscarFuncionarioPeloId(@PathVariable("idFuncionario") Long idFuncionario) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(funcionarioRepository.findById(idFuncionario));
+    }
+    
     
 }
