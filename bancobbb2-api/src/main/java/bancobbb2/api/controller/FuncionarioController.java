210baa8133606a7566242aa1b3d73766d053d5f8
Fabiana <fabiana.lua@gmail.com>
2024-03-22 23:53:16 -0300

    BBB-003 Criação do endpoint POST para cadastrar funcionario na classe FuncionarioRepository

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/FuncionarioController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/FuncionarioController.java
@@ -1,9 +1,14 @@
 package bancobbb2.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bancobbb2.api.model.Funcionario;
 import bancobbb2.api.repository.FuncionarioRepository;
 
 @RestController
@@ -12,5 +17,15 @@ public class FuncionarioController {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
+    
+    //Para cadastrar um funcionario
+    @PostMapping
+    public ResponseEntity<Funcionario> cadastrarFuncionario(@RequestBody Funcionario funcionario) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+        .body(funcionarioRepository.save(funcionario));
+
+    }
+
+    
     
 }
