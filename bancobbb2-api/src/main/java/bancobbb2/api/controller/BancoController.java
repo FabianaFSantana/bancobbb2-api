2e6a5fb919b8dea5f65186b6c547972a64e50420
Fabiana <fabiana.lua@gmail.com>
2024-03-22 21:22:10 -0300

    BBB-002 Criação do método Delete para excluir um banco na classe BancoController

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/BancoController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/BancoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,6 +72,15 @@ public class BancoController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    //Método para deletar banco:
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluirBanco(@PathVariable("id") Long id) {
+        bancoRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+        .body("Banco excluído com sucesso!");
+
     }
 
 
