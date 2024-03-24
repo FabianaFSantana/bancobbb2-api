e01dd93b0a776ff8b81a815a46615cfe1cd2601a
Fabiana <fabiana.lua@gmail.com>
2024-03-24 20:25:31 -0300

    BBB-006 Criação dos endpoints GET para exibir lista de contas poupanca e buscar conta pelo id na classe ContaPoupacaController

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaPoupancaController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaPoupancaController.java
@@ -1,8 +1,13 @@
 package bancobbb2.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +29,20 @@ public class ContaPoupancaController {
         .body(contaPoupancaRepository.save(contaPoupanca));
     }
 
+    @GetMapping
+    public ResponseEntity<List<ContaPoupanca>> exibirListaDeContasPoupanca() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(contaPoupancaRepository.findAll());
+    }
+
+    @GetMapping("/{idCp}")
+    public ResponseEntity<Optional<ContaPoupanca>> buscarContaPoupancaPeloId(@PathVariable("idCp") Long idCp) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(contaPoupancaRepository.findById(idCp));
+    }
+
     
+
+
     
 }
