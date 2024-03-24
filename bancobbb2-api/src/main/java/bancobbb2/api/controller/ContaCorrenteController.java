b2370d48951165f6ec7f1ca58765a02bf517b584
Fabiana <fabiana.lua@gmail.com>
2024-03-24 18:21:18 -0300

    BBB-005 Criação dos endpoints GET para exibir lista de contas e buscar conta corrente pelo id

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaCorrenteController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaCorrenteController.java
@@ -1,8 +1,13 @@
 package bancobbb2.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +23,26 @@ public class ContaCorrenteController {
     @Autowired
     private ContaCorrenteRepository contaCorrenteRepository;
 
+    //Para cadastrar uma conta corrente:
     @PostMapping
     public ResponseEntity<ContaCorrente> cadastrarContaCorrente(@RequestBody ContaCorrente contaCorrente) {
         return ResponseEntity.status(HttpStatus.CREATED)
         .body(contaCorrenteRepository.save(contaCorrente));
     }
+
+    //Para exibir lista de todas as contas cadastradas:
+    @GetMapping
+    public ResponseEntity<List<ContaCorrente>> exibirListaDeContas() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(contaCorrenteRepository.findAll());
+    }
+
+    //Para exibir conta corrente pelo id:
+    @GetMapping("/{idCc}")
+    public ResponseEntity<Optional<ContaCorrente>> buscarContaCorrentePeloId(@PathVariable("idCc") Long idCc) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(contaCorrenteRepository.findById(idCc));
+    }
+
     
 }
