01a23efb2d54d60c446bd3a0933fb1bacac38dc7
Fabiana <fabiana.lua@gmail.com>
2024-03-25 08:40:01 -0300

    BBB-007 Criação dos Endpoints PUT e DELETE para atualizar e excluir uma contasalário localizada pelo id

--- bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaSalarioController.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/controller/ContaSalarioController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,32 @@ public class ContaSalarioController {
     public ResponseEntity<Optional<ContaSalario>> buscarContaPeloId(@PathVariable("idCs") Long idCs) {
         return ResponseEntity.status(HttpStatus.OK)
         .body(contaSalarioRepository.findById(idCs));
+    }
+
+    @PutMapping("/{idCs}")
+    public ResponseEntity<ContaSalario> atualizarDadosDaConta(@PathVariable("idCs") Long idCs,
+    @RequestBody ContaSalario contaSalario) {
+
+        Optional<ContaSalario> contaOptional = contaSalarioRepository.findById(idCs);
+        
+        if (contaOptional.isPresent()) {
+            ContaSalario contaEncontrada = contaOptional.get();
+
+            contaEncontrada.setNumeroCs(contaSalario.getNumeroCs());
+            contaEncontrada.setSenhaCs(contaSalario.getSenhaCs());
+            contaEncontrada.setSaldoCs(contaSalario.getSaldoCs());
+
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(contaSalarioRepository.save(contaEncontrada));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{idCs}")
+    public ResponseEntity<String> cancelarContaSalario(@PathVariable("idCs") Long idCs) {
+        contaSalarioRepository.deleteById(idCs);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Conta Salario cancelada com sucesso!");
     }
 
 
