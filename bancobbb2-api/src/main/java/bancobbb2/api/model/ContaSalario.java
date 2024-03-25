4f69c86cba82bf4271be17130aabbd160bb06dd6
Fabiana <fabiana.lua@gmail.com>
2024-03-25 07:53:38 -0300

    BBB-007 Criação da classe model ContaSalario

---
+++ bancobbb2-api/src/main/java/bancobbb2/api/model/ContaSalario.java
@@ -0,0 +1,31 @@
+package bancobbb2.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "contaSalario")
+
+public class ContaSalario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idCs;
+
+    @Column(nullable = false, unique = true)
+    private int numeroCs;
+
+    @Column(nullable = false)
+    private String senhaCS;
+
+    @Column(nullable = false)
+    private double saldoCs;
+}
