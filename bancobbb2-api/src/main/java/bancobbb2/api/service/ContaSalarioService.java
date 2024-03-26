610973ef5729e18e06c10e551696c7949ac2c57b
Fabiana <fabiana.lua@gmail.com>
2024-03-25 23:18:42 -0300

    BBB-010 Relacionamento entre funcionario e conta salário

--- bancobbb2-api/src/main/java/bancobbb2/api/service/ContaSalarioService.java
+++ bancobbb2-api/src/main/java/bancobbb2/api/service/ContaSalarioService.java
@@ -1,13 +1,17 @@
 package bancobbb2.api.service;
 
+import java.lang.module.ResolutionException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scripting.config.LangNamespaceHandler;
 import org.springframework.stereotype.Service;
 
 import bancobbb2.api.model.ContaSalario;
+import bancobbb2.api.model.Funcionario;
 import bancobbb2.api.model.Usuario;
 import bancobbb2.api.repository.ContaSalarioRepository;
+import bancobbb2.api.repository.FuncionarioRepository;
 import bancobbb2.api.repository.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -23,6 +27,9 @@ public class ContaSalarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
 
     //Método para associar usuário a conta salário.
     public void associarContaSalarioUsuario(Long idUsuario, Long idCs) {
@@ -40,6 +47,23 @@ public class ContaSalarioService {
         } else {
             throw new EntityNotFoundException("Usuário não encontrado!");
         }
+    }
+
+    //Método para associar funcionário a conta salário.
+    public void associarFuncContaSalario(Long idFuncionario, Long idCs) {
+
+        Optional<Funcionario> funcOptional = funcionarioRepository.findById(idFuncionario);
+        Optional<ContaSalario> contaOptional = contaSalarioRepository.findById(idCs);
+
+        if (funcOptional.isPresent() && contaOptional.isPresent()) {
+            Funcionario funcionario = funcOptional.get();
+            ContaSalario contaSalario = contaOptional.get();
+
+            funcionario.setContaSalario(contaSalario);
+            funcionarioRepository.save(funcionario);
+        }
+        throw new EntityNotFoundException("Funcionário não encontrado.");
+
     }
     
 }
